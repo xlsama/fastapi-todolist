40fa9242721e4c3f244a8d177ccfcbb3c95e3ac0
xlsama <im.xlsama@gmail.com>
2025-11-17 11:56:36 +0800

    chore: update

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 ## 启动
 
+## 开发环境，自动热重载
+
 ```shell
-uvicorn main:app --reload
+fastapi dev
+```
+
+### 生产环境
+
+```shell
+fastapi run
 ```
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from typing import Union
-
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+from enum import Enum
 
 app = FastAPI()
 
@@ -11,6 +10,12 @@ class Item(BaseModel):
     name: str
     price: float
     is_offer: Union[bool, None] = None
+
+
+class FruitName(str, Enum):
+    apple = "apple"
+    banana = "banana"
+    orange = "orange"
 
 
 @app.get("/")
@@ -26,3 +31,14 @@ async def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+
+@app.get("/fruits/{fruit_name}")
+async def get_fruit(fruit_name: FruitName):
+    if fruit_name is FruitName.apple:
+        return {"fruit_name": fruit_name, "message": "Apple is good for your health"}
+
+    if fruit_name is FruitName.banana:
+        return {"fruit_name": fruit_name, "message": "Banana is good for your health"}
+
+    return {"fruit_name": fruit_name, "message": "Orange is good for your health"}
